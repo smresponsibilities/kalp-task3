907ae05ce13c8d73c69d8a716adc79da1255e00f
smresponsibilities <sm.responsibilities@gmail.com>
2024-09-20 19:40:29 +0530

    feat: did the basic task

---
+++ junior/frontend/src/hooks/useKalpAPI.tsx
@@ -0,0 +1,73 @@
+"use client"
+import { useState } from 'react';
+
+export const useKalpApi = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  const callApi = async (endpoint: string, args : { [key: string]: any }) => {
+    setError(null);
+    const params = {
+      network: 'TESTNET',
+      blockchain: 'KALP',
+      walletAddress: '6b99a3b9268c0fd78b438a0f532e1e4ebb79fb81',
+      args: args,
+    };
+
+    try {
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': apiKey!,
+        },
+        body: JSON.stringify(params),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || 'Something went wrong');
+      }
+      setLoading(false);
+      return data;
+    } catch (err : any) {
+      setError(err);
+      setLoading(false);
+      throw err;
+    }
+  };
+
+  const claim = async (address : string) => {
+    setLoading(true);
+    const endpoint =
+      'https://gateway-api.kalp.studio/v1/contract/kalp/invoke/P1tsj2naimR7SfhaSF50zfj6VayC1LM41726821678510/Claim';
+    const args = {
+      amount: 100,
+      address: address,
+    };
+    return callApi(endpoint, args);
+  };
+
+  const balanceOf = async (account : string) => {
+    const endpoint =
+      'https://gateway-api.kalp.studio/v1/contract/kalp/query/P1tsj2naimR7SfhaSF50zfj6VayC1LM41726821678510/BalanceOf';
+    const args = {
+      account: account,
+    };
+    return callApi(endpoint, args);
+  };
+
+  const totalSupply = async () => {
+    const endpoint =
+      'https://gateway-api.kalp.studio/v1/contract/kalp/query/P1tsj2naimR7SfhaSF50zfj6VayC1LM41726821678510/TotalSupply';
+    const args = {};
+    return callApi(endpoint, args);
+  };
+
+  return { claim, balanceOf, totalSupply, loading, error };
+};
+
+
